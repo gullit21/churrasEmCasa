0cc4edb5519a6f9e5db5d483fbfa1810bcfbe2e8
Gullit <gullit.menezes@gmail.com>
2021-05-30 02:31:37 -0300

    final aula

--- componentes/Topo/index.js
+++ componentes/Topo/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 
 import churrasco from '../../assets/churrasco.png';
+import estilo from './estilo.js';
 
 export default function Topo() {
   return (
-    <View>
-      <Image source = { churrasco } />
+    <View style = {estilo.box}>
+      <Image style = { estilo.boxIcone } source = { churrasco } />
 
       <View>
-        <Text>Churrasco em casa</Text>
-        <Text>Calculando a comida e a bebida</Text>
+        <Text style = {estilo.boxTitulo}>Churrasco em casa</Text>
+        <Text style = {estilo.boxSubtitulo}>Calculando a comida e a bebida</Text>
       </View>
     </View>
   )
